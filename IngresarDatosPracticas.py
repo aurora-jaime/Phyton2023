b19c00c76fbfe16a8a34c2231941e4c648b401ae
Aurora Jaime <aurora-jaime@gmail.com>
2023-06-02 15:46:23 -0600

    Ingresar Datos

---
+++ IngresarDatosPracticas.py
@@ -0,0 +1,11 @@
+nombre = input("Ingresa tu nombre:")
+edad = input("Ingresa tu edad:")
+
+print (nombre)
+print (edad)
+
+print("El nombre es: " + (nombre) + "y la edad es: "+str(edad))
+
+#CONCATENACION
+#https://j2logo.com/como-concatenar-y-formatear-strings/
+
