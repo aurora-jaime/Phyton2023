fde993cd2808ae5d0e7783e56a93e53a5f2b4d1b
Aurora Jaime <aurora-jaime@gmail.com>
2023-06-02 15:33:28 -0600

    Precio

--- Manzanas.py
+++ Manzanas.py
@@ -1,6 +1,8 @@
-precioDeManzana: int = 10
-cantidadDeManzana: int = 5
+precioDeManzana = 5
+cantidadDeManzana = int(input("¿Cuantas manzanas compraste?")) 
+#input recibe valores de tipo string
+# el int antes de el input es para que nos pregunte solo numeros enternos 
+
 
 print("Va a pagar:", precioDeManzana * cantidadDeManzana)
-
 
