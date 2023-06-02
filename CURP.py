181fdc61c7e2021bc1ad83d0838046b966279627
Aurora Jaime <aurora-jaime@gmail.com>
2023-06-02 13:54:46 -0600

    Variables

---
+++ CURP.py
@@ -0,0 +1,6 @@
+#NOMBRE / APELLIDO PATERNO / APELLIDO MATERNO / FECHA DE NACIMIENTO 
+
+nombre = print(input("Ingresa nombre:"))
+ape_paterno = print(input("Ingresa el apellido paterno:"))
+ape_materno = print(input("Ingresa el apellido materno:"))
+
