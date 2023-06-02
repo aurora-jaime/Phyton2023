5313ec5247fdd1b62f3c40fd8846f4809157b0df
Aurora Jaime <aurora-jaime@gmail.com>
2023-06-02 14:09:16 -0600

    Hola mundito

--- CURP.py
+++ CURP.py
@@ -3,4 +3,14 @@
 nombre = print(input("Ingresa nombre:"))
 ape_paterno = print(input("Ingresa el apellido paterno:"))
 ape_materno = print(input("Ingresa el apellido materno:"))
+fecha_naci = print(input("Ingresa la fecha de nacimiento:"))
 
+nombre_completo = ape_materno + ape_paterno
+string = nombre_completo
+words = string.split(' ') 
+character = ''
+
+for word in words:
+   character += word[0]
+
+print(character)
