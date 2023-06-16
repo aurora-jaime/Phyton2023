bbc3fefdb22cd730401be646ede93eafbc352997
Aurora Jaime <aurora-jaime@gmail.com>
2023-06-16 12:30:13 -0600

    Homo

--- CURP.py
+++ CURP.py
@@ -9,6 +9,9 @@ JA G A 011215 XN9
  1          2 
  jaime     gutierrez
 '''
+import random #librerias 
+import string
+
 nombre = input("Ingresa tu nombre:")
 primer_apellido = input("Ingresa tu apellido paterno:")
 segundo_apellido = input("Ingresa tu apellido materno:")
@@ -18,10 +21,16 @@ dia = int(input("Ingresa tu día de nacimiento:"))
 
 ape_pa = primer_apellido
 ape_ma = segundo_apellido 
+ultimos_dos_digitos = ano % 100
 
+def homoclave():
+    letras = string.ascii_uppercase
+    num = string.digits
+    homo = ''.join(random.choices(letras, k=3))
+    homo += ''.join(random.choices(num, k=2))
 
 print("\tAqui se va juntando la curp:  ")
-print(ape_pa[:2].upper(), ape_ma[:1].upper(), nombre[:1].upper(), ano[:2], mes, dia)
+print(ape_pa[:2].upper(), ape_ma[:1].upper(), nombre[:1].upper(), ultimos_dos_digitos, mes, dia, homoclave(homoclave))
 
 
 
